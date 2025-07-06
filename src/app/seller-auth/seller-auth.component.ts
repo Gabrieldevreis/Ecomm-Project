8ebf325544947e12056a0d3554c1a9bd7105de43
gabrieldevreis <gabrielreisdev@gmail.com>
2025-07-06 18:40:32 -0300

    feat: add seller home component and update routing; implement seller sign-up functionality

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { signUp } from '../data-type';
+import { Router} from '@angular/router'
 
 @Component({
   selector: 'app-seller-auth',
@@ -6,10 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './seller-auth.component.css'
 })
 export class SellerAuthComponent {
-  constructor(){}
+  constructor(private seller: SellerService, private router: Router){}
 
   ngOnInit() :void{}
-  signUp(data:object):void{
+  signUp(data:signUp):void{
       console.warn(data)
-    }
+      this.seller.userSignUp(data).subscribe((result)=>{
+        if(result){
+          this.router.navigate(['seller-home'])
+        }
+      })
+    }    
 }
